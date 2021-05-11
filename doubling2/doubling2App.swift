1de9465a922d2e1d95e2c58dc2afe381db05cf34
zungwooker <zungwooker@gmail.com>
2021-05-11 14:12:15 +0900

    Initial Commit

---
+++ doubling2/doubling2App.swift
@@ -0,0 +1,17 @@
+//
+//  doubling2App.swift
+//  doubling2
+//
+//  Created by blair on 2021/05/11.
+//
+
+import SwiftUI
+
+@main
+struct doubling2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
